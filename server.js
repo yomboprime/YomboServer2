096e570b67744b65ca5f2b49776eb1a460095e3a
yomboprime <yomboprime@users.noreply.github.com>
2020-09-18 16:02:04 +0200

    Test server

--- server.js
+++ server.js
@@ -1,32 +1,163 @@
 
-// we've started you off with Express (https://expressjs.com/)
-// but feel free to use whatever libraries or frameworks you'd like through `package.json`.
-const express = require("express");
-const app = express();
+// - Requires -
 
-// our default array of dreams
-const dreams = [
-  "Find and count some sheep",
-  "Climb a really tall mountain",
-  "Wash the dishes"
-];
+var Peer;
+var wrtc;
 
-// make all the files in 'public' available
-// https://expressjs.com/en/starter/static-files.html
-app.use(express.static("public"));
+var network = require( './server/network.js' );
+//var sendToClientsJSON = network.sendToClientsJSON;
+//var sendToClientsJSONBinary = network.sendToClientsJSONBinary;
 
-// https://expressjs.com/en/starter/basic-routing.html
-app.get("/", (request, response) => {
-  response.sendFile(__dirname + "/views/index.html");
-});
+var fs = require( 'fs' );
 
-// send the default array of dreams to the webpage
-app.get("/dreams", (request, response) => {
-  // express helps us take JS objects and send them as JSON
-  response.json(dreams);
-});
 
-// listen for requests :)
-const listener = app.listen(process.env.PORT, () => {
-  console.log("Your app is listening on port " + listener.address().port);
-});
+// - Global variables -
+
+// App
+
+var app = null;
+var httpServer = null;
+//var wss = null;
+var peer = null;
+
+var isAppEnding = false;
+
+// Audio
+
+var audioConnected = false;
+var micAudioSource = null;
+var micMediaStreamTrack = null;
+var micMediaStream = null;
+
+
+// - Main code -
+
+initServer();
+
+// - End of main code -
+
+
+// - Functions -
+
+function initServer() {
+
+	process.on( "SIGINT", function() {
+
+		console.log( "  SIGINT Signal Received, shutting down" );
+
+		beginAppTermination();
+
+	} );
+
+//	connectVideoProcess();
+
+//	connectAudioProcess();
+
+	startServer( 8080/*process.env.PORT*/, function () {
+
+		console.log( "Server started." );
+
+	} );
+
+}
+
+function beginAppTermination() {
+/*
+	if ( videoConnected ) {
+
+		isAppEnding = true;
+		videoIsValid = false;
+
+		disconnectVideoProcess();
+
+	}
+	else {
+*/
+		finish();
+
+//	}
+
+}
+
+function finish() {
+/*
+	disconnectSerial( function () {
+
+		closeFilesSaveToDisk();
+*/
+		console.log( "YomboServer2 terminated successfully. Have a nice day." );
+
+		process.exit( 0 );
+/*
+	} );
+*/
+}
+
+function connectRTC() {
+
+	// Start peer to send audio stream over WebRTC
+	peer = new Peer( {
+		initiator: true,
+		trickle: false,
+		config: {
+			//iceServers: [ { urls: 'stun:stun.l.google.com:19302' } ],
+			/*
+			portRange: {
+				min: 8100,
+				max: 8200
+			}*/
+		},
+
+		wrtc: wrtc
+	} );
+
+	//peer.addStream( micMediaStream );
+
+	peer.on( 'connect', function () {
+
+		console.log( "RTC Peer connected" );
+
+	} );
+
+	// WebRTC signaling
+// TODO COMENTAR
+	peer.on( 'signal', function ( signal ) {
+
+		console.log( "**** RTC signal: " + JSON.stringify( signal ) + "************" );
+
+		//client.socket.send( JSON.stringify( signal ), function nop () {} );
+
+	} );
+
+	peer.on( 'error', function ( error ) {
+
+		console.log( "Error connecting via RTC to client: " + error );
+
+	} );
+
+	peer.on( 'close', function ( signal ) {
+
+		peer.destroy();
+		peer = null;
+
+	} );
+
+	peer.on( 'data', function ( message) {
+
+		onClientRTCDataMessage( client, message );
+
+	} );
+}
+
+function disconnectRTC() {
+
+	if ( peer ) peer.destroy();
+	peer = null;
+
+}
+
+function onClientRTCDataMessage( client, message ) {
+
+	console.log( "RTC Client Data Message: " + message );
+
+}
---
+++ server/network.js
@@ -0,0 +1,199 @@
+
+var MAX_URL_LENGTH = 2000;
+
+if ( typeof module !== undefined ) {
+
+	module.exports = {
+		startServer: startServer,
+		//sendToClientsJSON: sendToClientsJSON,
+		//sendToClientsJSONBinary: sendToClientsJSONBinary,
+		//removeClient: removeClient,
+		getClientParameters: getClientParameters,
+		getURLParameters: getURLParameters
+		//getNumClients: getNumClients
+	};
+
+}
+
+var express = require( 'express' );
+//var WebSocket = require( 'ws' );
+//var http = require( 'http' );
+var pathJoin = require( 'path' ).join;
+
+var app;//, server, wss;
+
+//var maxNumberOfClients = 10;
+
+//var nextClientId = 0;
+
+//var clients = [ ];
+
+//var onClientMessageInternal = null;
+//var onClientConnectedInternal = null;
+//var onClientDisconnectedInternal = null;
+
+function startServer( listenPort, onStarted ) {
+
+	// Create server
+	app = express();
+	server = http.Server( app );
+
+	//wss = new WebSocket.Server( { server: server } );
+
+	// Configure server
+
+	// Serve all public content
+	app.use( "/", express.static( pathJoin( __dirname, "../public" ) ) );
+
+	app.get( "/", ( request, response ) => {
+
+		response.sendFile( pathJoin( __dirname, "../views/index.html" ) );
+
+	} );
+
+	// Setup connection with socket.io clients
+	//wss.on( 'connection', onClientConnection );
+
+	//onClientConnectedInternal = onClientConnected1;
+	//onClientDisconnectedInternal = onClientDisconnected1;
+	//onClientMessageInternal = onClientMessage1;
+
+	// Start listening
+	//server.listen( listenPort, onStarted );
+
+	var listener = app.listen( listenPort, function () {
+
+		console.log( "******* YEP " );
+
+		onStarted();
+
+	});
+
+
+}
+
+/*
+function sendToClientsJSON( messageJSON ) {
+
+	for ( var i = 0, il = clients.length; i < il; i ++ ) {
+
+		clients[ i ].socket.send( messageJSON, function nop() {} );
+
+	}
+
+}
+
+function sendToClientsJSONBinary( messageJSON, messageBinary ) {
+
+	for ( var i = 0, il = clients.length; i < il; i ++ ) {
+
+		var socket = clients[ i ].socket;
+
+		socket.send( messageJSON, function nop() {} );
+		socket.send( messageBinary, function nop() {} );
+
+	}
+
+}
+
+function removeClient( client, reasonString ) {
+
+	client.socket.close( 0, reasonString );
+
+}
+*/
+
+function getClientParameters( req ) {
+
+	return getURLParameters( req.headers.referer );
+
+}
+
+function getURLParameters( url ) {
+
+	var params = [];
+
+	url = decodeURI( url );
+
+	if ( url.length > MAX_URL_LENGTH ) {
+
+		console.error( "URL exceeds max length. Aborting." );
+		return params;
+
+	}
+
+	var index = url.indexOf( "?" );
+	if ( index >= 0 ) {
+		var paramString = url.substring( index + 1 );
+		var paramStringArray = paramString.split( "&" );
+		for ( var i = 0; i < paramStringArray.length; i ++ ) {
+			var p = paramStringArray[ i ];
+			var index2 = p.indexOf( "=" );
+			if ( index2 >= 0 ) {
+				params.push( {
+					name: p.substring( 0, index2 ),
+					value: p.substring( index2 + 1 )
+				} );
+			}
+		}
+
+	}
+
+	return params;
+
+}
+
+// Main client connection function
+/*
+function onClientConnection( socket, req ) {
+
+	var client = {
+		socket: socket,
+		id: nextClientId ++
+	};
+
+	if ( clients.length > maxNumberOfClients ) {
+
+		console.log( "Client rejected." );
+		removeClient( client, "Too many clients, sorry." );
+		return;
+
+	}
+
+	clients.push( client );
+
+	console.log( "Client connected. (total: " + clients.length + ")" );
+
+	//socket.heartbeatTimeout = this.config.connectionTimeout;
+
+	socket.on( "close", function( msg ) {
+
+		var index = clients.indexOf( client );
+		if ( index >= 0 ) {
+
+			clients.splice( index, 1 );
+
+		}
+
+		onClientDisconnectedInternal( client );
+
+		console.log( "Client disconnected. (total: " + clients.length + ")" );
+
+	} );
+
+	socket.on( "message", function ( message ) {
+
+		onClientMessageInternal( client, message );
+
+	} );
+
+	onClientConnectedInternal( client );
+
+}
+
+function getNumClients() {
+
+	return clients.length;
+
+}
+*/
